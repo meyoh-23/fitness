ccd7fb338b3160c028da9ae92d8e4ff8d5d23ea2
Felix Ouma <moxiemeyoh@gmail.com>
2023-12-05 11:25:19 +0300

    add: tailwind configuration & Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-
+import Navbar from "@/scenes/navbar";
 
 function App() {
-  
 
   return (
     <>
-      <div className="w-full bg-primary">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ex illo pariatur mollitia alias ut voluptatem nesciunt? Officiis maxime, aspernatur nisi voluptatum illum dolor iste et veniam sed. Unde, quam?
-        </p>
+      <div className="bg-gray-20">
+        <Navbar/>
       </div>
     </>
   )
