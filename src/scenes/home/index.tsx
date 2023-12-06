e3f3a3e88bf259b48a1c824d75bcb456486d8dd7
Felix Ouma <moxiemeyoh@gmail.com>
2023-12-06 10:52:19 +0300

    add: animation with Framer

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -46,7 +46,16 @@ const Home = ({setSelectedPage}: Props) => {
             </p>
           </motion.div>
            {/* ACTIONS */}
-          <div className="mt-8 flex items-center gap-8 md:justify-start">
+          <motion.div className="mt-8 flex items-center gap-8 md:justify-start"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5}}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: {opacity: 1, x: 0 }
+            }}
+          >
           <ActionButton setSelectedPage={setSelectedPage} children="Join Now"/>
           <AnchorLink
           className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
@@ -55,7 +64,7 @@ const Home = ({setSelectedPage}: Props) => {
           >
             <p>Learn More</p>
           </AnchorLink>
-        </div>
+        </motion.div>
         </div>
 
          {/* IMAGE */}
